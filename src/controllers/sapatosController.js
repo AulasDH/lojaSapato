67157b120ea32163120667d1b9f64395bad729d8
tsunodajapa <gabrieltsunoda@gmail.com>
2022-03-09 21:44:11 -0300

    feat: cadastra sapatos

--- src/controllers/sapatosController.js
+++ src/controllers/sapatosController.js
@@ -7,6 +7,30 @@ const sapatosController = {
       title: 'Express',
     });
   },
+  create: (request, response) => {
+    const nomeArquivosSapatos = 'sapatos.json';
+
+    // busquei o arquivo sapatos.json
+    const sapatosArquivo = fs.readFileSync(nomeArquivosSapatos);
+
+    // transformei string em JSON
+    const sapatosJSON = JSON.parse(sapatosArquivo);
+
+    // criei um novo objeto com todo cadastro + um novo UUID (identificador)
+    const novoSapato = {
+      id: uuid(),
+      ...request.body //{nome: teste} -> {nome:teste} 
+    }
+
+    // inserindo meu novo sapato no array de sapatos
+    sapatosJSON.push(novoSapato);
+
+    // grava todos os sapatos no arquivo
+    fs.writeFileSync(nomeArquivosSapatos, JSON.stringify(sapatosJSON))
+
+    //redireciono para tela inical
+    response.redirect('/');
+  },
 }
 
 // usuario chama uma url com metodo
